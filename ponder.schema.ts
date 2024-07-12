492040cc636b0fd10c88f7410ac5d54599a1c0e9
nuts-rice <red44443@gmail.com>
2024-07-12 11:34:57 -0400

    🗃️ board schema

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,7 +1,10 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  LlamaCoreInstance: p.createTable({
+  board: p.createTable({
+    grid: p.json(),
+    users: p.string().list(),
     id: p.string(),
+    last_evolved_at: p.int(),
   }),
 }));
